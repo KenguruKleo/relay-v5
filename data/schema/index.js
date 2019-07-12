890b68727e62232b72c6f78a74b5ada1dc607e27
konstantin.yemelianov <konstantin.yemelianov@uberall.com>
2019-07-12 13:50:20 +0200

    add first schema

---
+++ data/schema/index.js
@@ -0,0 +1,18 @@
+// @flow
+
+import {GraphQLObjectType, GraphQLSchema} from 'graphql';
+
+import {nodeField} from './nodes.js';
+import {UserQuery} from './queries/UserQuery';
+
+const Query = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        user: UserQuery,
+        node: nodeField,
+    },
+});
+
+export const schema = new GraphQLSchema({
+    query: Query,
+});
